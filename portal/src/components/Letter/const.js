421f89451264be34d762298c463243c4fa327a68
lộc nguyễn <locnd.blackwind@gmail.com>
2021-09-01 16:35:38 +0700

    task letter : complete

---
+++ portal/src/components/Letter/const.js
@@ -0,0 +1,174 @@
+import {
+  configTableOptions,
+  configIDRowTable,
+  numberFormat,
+  getPropsConfigTable,
+} from "../../utils/index";
+import { CheckAccess } from "../../navigation/VerifyAccess";
+import React from "react";
+import { Button } from "reactstrap";
+import { Checkbox } from "antd";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import * as yup from "yup";
+export const initialValues = {
+  letter_id: "",
+  letter: "",
+  number: "",
+  is_vowel: 0,
+  is_active: 0,
+  desc: "",
+};
+///// validate
+export const validationSchema = yup.object().shape({
+  letter: yup
+    .string()
+    .required("Chữ cái không được để trống .")
+    .max(1, "Chữ cái tối đa 1 kí tự .")
+    .matches(/^[A-Za-z]+$/, "Chữ cái chỉ được nhập chữ .")
+    .nullable(),
+  number: yup.number().required("Số không được để trống .").nullable(),
+});
+export const getColumTable = (data, total, query, handleDelete, handleReply, handleReview) => {
+  // console.log(data);
+  return [
+    configIDRowTable("letter_id", "/letter/detail/", query),
+    {
+      name: "letter",
+      label: "Chữ cái",
+      options: {
+        filter: false,
+        sort: true,
+        customHeadRender: (columnMeta, handleToggleColumn) => {
+          return (
+            <th key={`head-th-${columnMeta.label}`} className="MuiTableCell-root MuiTableCell-head">
+              <div className="text-center">{columnMeta.label}</div>
+            </th>
+          );
+        },
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return <div className="text-center">{value}</div>;
+        },
+      },
+    },
+    {
+      name: "is_vowel",
+      label: "Nguyên âm",
+      options: {
+        filter: false,
+        sort: true,
+        customHeadRender: (columnMeta, handleToggleColumn) => {
+          return (
+            <th key={`head-th-${columnMeta.label}`} className="MuiTableCell-root MuiTableCell-head">
+              <div className="text-center">{columnMeta.label}</div>
+            </th>
+          );
+        },
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <div className="text-center">
+              <Checkbox checked={value == 1}></Checkbox>
+            </div>
+          );
+        },
+      },
+    },
+
+    {
+      name: "number",
+      label: "Số",
+      options: {
+        filter: false,
+        sort: true,
+        customHeadRender: (columnMeta, handleToggleColumn) => {
+          return (
+            <th key={`head-th-${columnMeta.label}`} className="MuiTableCell-root MuiTableCell-head">
+              <div className="text-center">{columnMeta.label}</div>
+            </th>
+          );
+        },
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return <div className="text-center">{value}</div>;
+        },
+      },
+    },
+    {
+      name: "is_active",
+      label: "Kích hoạt",
+      options: {
+        filter: false,
+        sort: true,
+        customHeadRender: (columnMeta, handleToggleColumn) => {
+          return (
+            <th key={`head-th-${columnMeta.label}`} className="MuiTableCell-root MuiTableCell-head">
+              <div className="text-center">{columnMeta.label}</div>
+            </th>
+          );
+        },
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <div className="text-center">
+              <Checkbox checked={value == 1}></Checkbox>
+            </div>
+          );
+        },
+      },
+    },
+    {
+      name: "desc",
+      label: "Mô tả",
+      options: {
+        filter: false,
+        sort: true,
+        customHeadRender: (columnMeta, handleToggleColumn) => {
+          return (
+            <th key={`head-th-${columnMeta.label}`} className="MuiTableCell-root MuiTableCell-head">
+              <div className="text-center">{columnMeta.label}</div>
+            </th>
+          );
+        },
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return <div className="text-center">{value}</div>;
+        },
+      },
+    },
+    {
+      name: "Thao tác",
+      options: {
+        filter: false,
+        sort: false,
+        empty: true,
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <div className="text-center">
+              <CheckAccess permission="MD_LETTER_EDIT">
+                <Button
+                  color="primary"
+                  title="Chỉnh sửa"
+                  className="mr-1"
+                  onClick={(evt) => {
+                    window._$g.rdr(`/letter/edit/${data[tableMeta["rowIndex"]].letter_id}`);
+                  }}
+                >
+                  <i className="fa fa-edit" />
+                </Button>
+              </CheckAccess>
+              <CheckAccess permission="MD_LETTER_DEL">
+                <Button
+                  color="danger"
+                  title="Xóa"
+                  className=""
+                  onClick={(evt) =>
+                    handleDelete(data[tableMeta["rowIndex"]].letter_id, tableMeta["rowIndex"])
+                  }
+                >
+                  <i className="fa fa-trash" />
+                </Button>
+              </CheckAccess>
+            </div>
+          );
+        },
+      },
+    },
+  ];
+};
